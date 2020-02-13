11f323fe4bba2aad1f7f8919ff0a0e76fc385e47
greenhornsr <srollins11279@gmail.com>
2020-02-13 14:44:58 -0800

    set up blockchain_container component as home route

---
+++ basic_wallet_p/basic_wallet/src/components/Blockchain_Container.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Blockchain_Container = () => {
+const [bcdata, setBcdata] = useState()
+
+function getData(){
+    const data = axios.get('')
+}
+    
+
+    return (
+        <>
+            <h1>Home Sweet Home</h1>
+        </>
+    )
+}
+
+export default Blockchain_Container
