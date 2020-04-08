3d5502fa0d4d7fc76b187adfda32955f282a632c
greenhornsr <srollins11279@gmail.com>
2020-04-08 15:28:09 -0700

    added comments to basicblockgp blockchain.py to help me follow and understand the code better.  Started working on clientminingp blockchain.py and miner.py

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -3,6 +3,7 @@ import requests
 
 import sys
 import json
+import random
 
 
 def proof_of_work(block):
@@ -13,7 +14,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, indent=4, sort_keys=True)
+    proof = 0
+    # proof = str(random.random())
+    while not valid_proof(block_string, proof):
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +33,12 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    result = bool(guess_hash[:6] == "000000")
+
+    return result
 
 
 if __name__ == '__main__':
